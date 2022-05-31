e6598e594f58283ffd1bf79d7f15fca7c33dc79f
angeljeff <angeljeff00@gmail.com>
2022-05-30 23:52:51 -0500

    cambios

--- servidor/build/keys.js
+++ servidor/build/keys.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'jeffersonsantana',
+        host: '67.207.92.81',
+        user: 'yander',
         password: 'jeffersonAngel2022+',
         database: 'marketplace'
     }
